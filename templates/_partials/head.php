4301794373cdb46f1d9436b26af4cecb8f70646f
Jan Oelze <janoelzelebt@googlemail.com>
2014-01-22 01:19:31 +0100

    renamed main css file

--- templates/_partials/head.php
+++ templates/_partials/head.php
@@ -1,3 +1,3 @@
 <meta charset=utf-8>
 <title><?= $config['app_title']; ?></title>
-<link rel="stylesheet" type="text/css" href="<?= $config['base_url']; ?>/assets/food-in.css">
+<link rel="stylesheet" type="text/css" href="<?= $config['base_url']; ?>/assets/food-in-x.css">
