3edc411543fa521ab7388e3abf91594c682a7419
Jan Oelze <janoelzelebt@googlemail.com>
2014-01-22 01:08:02 +0100

    oops

--- templates/_partials/footer.php
+++ templates/_partials/footer.php
@@ -2,5 +2,5 @@
 
 <i>
   <p>A list of Recommendations.</p>
-  <p>Code on <a target="_blank" href="https://github.com/janoelze/food-in">GitHub</a></p>
+  <p>Code on <a target="_blank" href="https://github.com/janoelze/food-in-x">GitHub</a></p>
 </i>
